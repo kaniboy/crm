92d81226a8867650b2f36636f07332798c182a01
kaniboy <1521804363@qq.com>
2020-08-28 21:51:29 +0800

    实现退出登录

--- js/index.js
+++ js/index.js
@@ -2,11 +2,22 @@ $(function(){
     init();
     let $plan = $.Callbacks();
 
+    //显示用户信息
     $plan.add((_,baseInfo)=>{
         $(".baseBox>span").html(`你好，${baseInfo.name || ''}`)
     })
     $plan.add((power)=>{
         console.log("渲染菜单:",power)
+    })
+
+    //退出登录
+    $(".baseBox>a").click(async function(){
+        let result = await axios.get("/user/signout")
+        if(result.code == 0){
+            window.location.href = "login.html"
+            return;
+        }
+        alert("网络不给力，稍后再试")
     })
 
     //是否携带cook，没有返回登录页面
